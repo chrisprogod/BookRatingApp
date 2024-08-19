69089be3a27b9ef7fbb08c2321ae4137e93a0d7b
Christian <cteguimtu620@gmail.com>
2024-08-19 12:55:22 -0400

    Initial Commit

---
+++ BookRating/BookRatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookRatingApp.swift
+//  BookRating
+//
+//  Created by Christian Teguim on 2024-08-19.
+//
+
+import SwiftUI
+
+@main
+struct BookRatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
