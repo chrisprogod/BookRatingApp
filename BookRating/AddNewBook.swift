7ce4b9af2bb3519222e3624da51604e69579eddc
Christian <cteguimtu620@gmail.com>
2024-08-20 13:58:22 -0400

    New View to add books

---
+++ BookRating/AddNewBook.swift
@@ -0,0 +1,18 @@
+//
+//  AddNewBook.swift
+//  BookRating
+//
+//  Created by Christian Teguim on 2024-08-20.
+//
+
+import SwiftUI
+
+struct AddNewBook: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddNewBook()
+}
