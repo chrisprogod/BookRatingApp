509904ed59873a696870c6814055b9e1d8efec39
Christian <cteguimtu620@gmail.com>
2024-08-21 12:21:14 -0400

    Rename file

---
+++ BookRating/AppInit.swift
@@ -0,0 +1,21 @@
+//
+//  BookRatingApp.swift
+//  BookRating
+//
+//  Created by Christian Teguim on 2024-08-19.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BookRatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BookListView()
+        }
+        .modelContainer(for: Book.self)
+    }
+    init() {
+        print(URL.applicationDirectory.path(percentEncoded: false))}
+}
